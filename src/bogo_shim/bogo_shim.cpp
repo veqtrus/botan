c2da7248529e84c97b65b367f5b8b057cf1ff3a2
Jack Lloyd <jack@randombit.net>
2020-11-11 05:56:30 -0500

    Remove outer shared_ptr from Certificate in certstore interface
    Since 2.4.0 X509_Certificate and X509_CRL have been internally
    shared so an other shared_ptr is just overhead and API complexity.
    Use std::optional for APIs where the object was optional.

--- src/bogo_shim/bogo_shim.cpp
+++ src/bogo_shim/bogo_shim.cpp
@@ -1324,7 +1324,7 @@ class Shim_Callbacks final : public Botan::TLS::Callbacks
          }
 
       void tls_verify_cert_chain(const std::vector<Botan::X509_Certificate>& /*cert_chain*/,
-                                 const std::vector<std::shared_ptr<const Botan::OCSP::Response>>& /*ocsp_responses*/,
+                                 const std::vector<std::optional<Botan::OCSP::Response>>& /*ocsp_responses*/,
                                  const std::vector<Botan::Certificate_Store*>& /*trusted_roots*/,
                                  Botan::Usage_Type /*usage*/,
                                  const std::string& /*hostname*/,
